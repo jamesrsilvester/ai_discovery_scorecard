3dd4199958cc46b9a69cedd1ca69c055440183f3
James Silvester <6411628+jamesrsilvester@users.noreply.github.com>
2026-01-20 14:57:30 -0700

    Fix next.config.js for GH Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'ai_discovery_scorecard';
+
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    basePath: isProd ? `/${repo}` : '',
+    assetPrefix: isProd ? `/${repo}/` : '',
+    images: {
+        unoptimized: true,
+    },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
